bec0561a7caa34bbe9ad236f6ad5ef94f7863734
Aaron de Boer <aarondeboerwk@gmail.com>
2022-10-19 18:17:26 +0200

    Made simple gameobjects for basic gameplay, made a follow camera using Cinemachine and made the movement of the rocket with Unity's new input system.

---
+++ Assets/Scripts/Controllable.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:801e0e9851f46f3f8b9a504c5daa986ac5e3a1862eaff1ded9601292991ea1d2
+size 1764
---
+++ Assets/Scripts/Input System Scripts/PlayerControls.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2a2442ef4ef7d5153ab754620838bfc6464abe155c5a329c8998b0d615cb9123
+size 8256
