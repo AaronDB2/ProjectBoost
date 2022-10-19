1e318f7817211b9a732beba605c8915d2b75fe87
Aaron de Boer <aarondeboerwk@gmail.com>
2022-10-19 21:33:26 +0200

    Added thrust sound effect, added simple loading between scenes and added collision check for player

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c979065ef4ea629b4b34e7987ea88fff59ae0e58d09818c507b279a4ad5ea808
+size 1345
--- Assets/Scripts/Controllable.cs
+++ Assets/Scripts/Controllable.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:801e0e9851f46f3f8b9a504c5daa986ac5e3a1862eaff1ded9601292991ea1d2
-size 1764
+oid sha256:2bda2c1e38710222319e100a46bbd3e0e46e04c206069e15fa4b897906f9675e
+size 2536
