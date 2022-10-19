621c3a972695dbfadc1b474526bb78dc399e7185
Aaron de Boer <aarondeboerwk@gmail.com>
2022-10-19 22:10:26 +0200

    Added crash and finish sound effects

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c979065ef4ea629b4b34e7987ea88fff59ae0e58d09818c507b279a4ad5ea808
-size 1345
+oid sha256:3e72c07355901cedce43f4619f708e7979f3cca21a94930fe7bb95a899ffd0ae
+size 1649
--- Assets/Scripts/Controllable.cs
+++ Assets/Scripts/Controllable.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2bda2c1e38710222319e100a46bbd3e0e46e04c206069e15fa4b897906f9675e
-size 2536
+oid sha256:36b214fa830ac5324e505a484d8bbcd5f35db19b6b69d90c1a9c1dc4fcea857c
+size 2975
